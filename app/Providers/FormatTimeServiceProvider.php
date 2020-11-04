35eb83649f12ed9764bf15fdc2660525a9d3f16f
Pablo Gonzalez <pabloandres6@gmail.com>
2020-11-03 21:59:42 -0500

    cambio include de formatTime

--- app/Providers/FormatTimeServiceProvider.php
+++ app/Providers/FormatTimeServiceProvider.php
@@ -13,7 +13,7 @@ class FormatTimeServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        require_once '/Helpers/FormatTime.php';
+        require_once app_path().'/helpers/FormatTime.php';
     }
 
     /**
