0e988330e74422686b1553651a4e59ea4dfbf2ab
Pablo Gonzalez <pabloandres6@gmail.com>
2020-11-11 10:28:47 -0500

    Edit route login api

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', 'AuthController@login');
+
 
 Route::group([
     'prefix' => 'auth'
 ], function () {
-    Route::post('login', 'AuthController@login');
+
     Route::post('signup', 'AuthController@signUp');
 
     Route::group([
