a31683ea2b805932170470298859059f78dbdb76
Pablo Gonzalez <pabloandres6@gmail.com>
2020-11-23 14:22:33 -0500

    Stable Index prestamos the API for APP

--- app/Http/Controllers/PrestamoApiController.php
+++ app/Http/Controllers/PrestamoApiController.php
@@ -35,48 +35,15 @@ class PrestamoApiController extends Controller
 
     public function index(Request $request)
     {
-    	//$user = response()->json($request->user());
-    	//return $user->prestamos;
 
 
-    	//$user = \Auth::user();
     	$user = $request->user();
     	$prestamos = $user->asPrestamos;
 
-    	 return response()->json(
+    	// result de api request con index correcto
+    	return response()->json(
             $prestamos
         );
-
-
-
-/*
-    	$prestamos = Prestamo::where('user_id', $user->id)->orderBy('id', 'desc')->get([
-			"id",
-			"user_id",
-			"name_user",
-			"surname_user",
-			"cedula_user",
-			"ciudad",
-			"bloque",
-			"direccion",
-			"estado",
-			"nombre_activo",
-			"serial",
-			"placa",
-			"salon",
-			"programa",
-			"celular",
-			"referencia",
-			"cantidad",
-			"salida_por",
-			"descripcion",
-			"editado_por",
-			"created_at",
-			"updated_at",
-    	]);
-    	*/
-
-
     }
 
     public function store()
