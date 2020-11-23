361f02aee3d448940eabd0227c6fb2aeee0ab94d
Pablo Gonzalez <pabloandres6@gmail.com>
2020-11-23 14:09:32 -0500

    Test index prestamos

--- app/Http/Controllers/PrestamoApiController.php
+++ app/Http/Controllers/PrestamoApiController.php
@@ -43,9 +43,10 @@ class PrestamoApiController extends Controller
     	$user = $request->user();
     	$prestamos = $user->asPrestamos;
 
-    	return response()->json([
-            'prestamos' => $prestamos
+    	 return response()->json([
+            $prestamos
         ]);
+
 
 
 /*
