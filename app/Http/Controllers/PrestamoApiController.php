fb95fe2357a6e06be26ca3802870776af35f8f31
Pablo Gonzalez <pabloandres6@gmail.com>
2020-12-27 11:49:46 -0500

    Edit method store

--- app/Http/Controllers/PrestamoApiController.php
+++ app/Http/Controllers/PrestamoApiController.php
@@ -79,16 +79,14 @@ class PrestamoApiController extends Controller
 
 	    //$success = $prestamo;
 
-	    return compact('success');
+	    //return compact('success');
 
 	    // result de api request con ? correcto
-
-	    /*
     	return response()->json(
             $success
-            //$prestamo
+
         );
-		*/
+
 	}
 
 }
