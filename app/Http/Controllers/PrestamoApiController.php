dbc66bcadbb8ab2950a562947083067797384fe5
Pablo Gonzalez <pabloandres6@gmail.com>
2020-12-27 11:05:26 -0500

    Test method store

--- app/Http/Controllers/PrestamoApiController.php
+++ app/Http/Controllers/PrestamoApiController.php
@@ -79,15 +79,16 @@ class PrestamoApiController extends Controller
 
 	    //$success = $prestamo;
 
-	    //return compact('success');
+	    return compact('success');
 
 	    // result de api request con ? correcto
 
-    	return response()->json([
-            'success' => $success
+	    /*
+    	return response()->json(
+            $success
             //$prestamo
-        ]);
-
+        );
+		*/
 	}
 
 }
