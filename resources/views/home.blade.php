72bec85dc8e3b23a310ce3c19e462ee2db1cf142
Pablo Gonzalez <pabloandres6@gmail.com>
2021-01-14 10:06:12 -0500

    Autofocus in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,8 +23,6 @@
   <div class="row justify-content-center">
     <div class="col-md-6">
 
-
-
       @if(session('message'))
       <div class="alert alert-success">
         {{ session('message') }}
@@ -32,13 +30,11 @@
       @endif
 
 
-
-
       <div class="row " style="display: flow-root;">
         <nav class="navbar navbar-light bg-light float-right" >
           <form class="form-inline">
-            <input name="buscarporpre" class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            <input name="buscarporpre" class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search" autofocus>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit" >Buscar</button>
           </form>
         </nav>
       </div>
