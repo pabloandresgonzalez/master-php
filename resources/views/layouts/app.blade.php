976c8a131870f0efc1821ebbe3426087164afa7e
Pablo Gonzalez <pabloandres6@gmail.com>
2021-01-14 13:57:00 -0500

    Logo css edition in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,10 +32,12 @@
       <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
 
-
-          <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="{{asset('images/logoucc.png')}}"/>
+          <div class="logonavbar">
+            <a class="navbar-brand" href="{{ url('/') }}">
+            <img class="imgnavbar" src="{{asset('images/logo-ucc.png')}}"/>
           </a>
+          </div>
+
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
           </button>
