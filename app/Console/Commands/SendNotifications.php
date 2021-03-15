9367de4d9e9889d4a0fdb1f5adde6d7190b30fbf
Pablo Gonzalez <pabloandres6@gmail.com>
2021-03-15 16:20:23 -0500

    edit fcm test4

--- app/Console/Commands/SendNotifications.php
+++ app/Console/Commands/SendNotifications.php
@@ -45,21 +45,32 @@ class SendNotifications extends Command
     {
         $this->info('Buscando prestamos con estado en curso');
 
-        //formato fecha                         2021-03-15 09:09:52
-        //fecha carbon                          2021-03-15 14:17:13.642210 America/Bogota (-05:00)
-        // fecha quitandole 8 dias a la actual  2021-01-13 00:00:00
-        //fecha prestamo bd                     2021-01-13 05:12:37
+        // fecha prestamo 2021-01-14 21:14:00 updated_at
+        // hora actual   2021-01-17 21:15:00
+        // updated_at
+        //$now = new \DateTime();
         $now = Carbon::now();
 
-        $prestamo = Prestamo::where('estado', 'En curso')
-                    ->where('created_at', '<', $now->copy()->subDay(8)->toDateString())
-                    ->get(['id', 'user_id'])->toArray();
 
-        dd($prestamo);
+        $headers = ['id', 'updated_at', 'user_id'];
 
+        $prestamosPentinetes = $this->getPrestamosMoreDays($now->copy());
+        $this->table($headers, $prestamosPentinetes->toArray());
+        //dd($prestamosPentinetes);
+
+        foreach ($prestamosPentinetes as $prestamo) {
+            $prestamo->name_user->sendFCM('El prestamo presenta mas de 3 dias con estado en curso.');
+            $this->info('Mensaje enviado al usuario (ID): '. $prestamo->user_id);
+        }
 
     }
 
+    private function getPrestamosMoreDays($now)
+    {
+        return Prestamo::where('estado', 'Terminado')
+                    ->where('updated_at', '<', $now->copy()->subDay(8)->toDateString())
+                    ->get(['id', 'updated_at', 'user_id']);
+    }
 
 
         /*
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
 
         //->everyMinute(); , ->hourly();
         $schedule->command('fcm:send')
-            ->everyFiveMinutes()
+            ->everyOneMinutes()
             ->appendOutputTo($filePath);
     }
 
